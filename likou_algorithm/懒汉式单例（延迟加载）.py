4c92129c133dc0d054931fa9cb7abf29303d32fc
jay <17798128831@163.com>
2024-04-05 11:29:30 +0800

    懒汉式单例

---
+++ likou_algorithm/懒汉式单例（延迟加载）.py
@@ -0,0 +1,24 @@
+import threading
+
+class Singleton:
+    _instance = None
+    _lock = threading.Lock()
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            with cls._lock:
+                if not cls._instance:
+                    cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+
+def demo():
+    # 使用
+    singleton = Singleton()
+    another_singleton = Singleton()
+    if singleton is another_singleton:  # 两次获取的对象是同一个对象
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    print(demo())
